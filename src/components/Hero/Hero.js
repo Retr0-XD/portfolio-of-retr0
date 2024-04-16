774a42f020b1e2ad4cafabf150e983d399c6a801
Retr0-XD <88275564+Retr0-XD@users.noreply.github.com>
2024-04-16 16:34:12 +0530

    Update Hero.js

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 
 import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <>
+const Hero = () => {
+  const router = useRouter();
+
+  const handleLearnMoreClick = () => {
+    router.push('https://leetcode.com/retr0sec');
+  };
+
+  return (
     <Section row nopadding>
       <LeftSection>
         <SectionTitle main center>
@@ -13,12 +20,12 @@ const Hero = (props) => (
           My Portfolio Website
         </SectionTitle>
         <SectionText>
-        “Imagination is more important than knowledge.  For knowledge is limited, whereas imagination embraces the entire world, stimulating progress, giving birth to evolution.”
+          “Imagination is more important than knowledge. For knowledge is limited, whereas imagination embraces the entire world, stimulating progress, giving birth to evolution.”
         </SectionText>
-        <Button onClick={props.handleClick}>Learn More</Button>
+        <Button onClick={handleLearnMoreClick}>Learn More</Button>
       </LeftSection>
     </Section>
-  </>
-);
+  );
+};
 
 export default Hero;
